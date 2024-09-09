ce31b99bc457925692cee552c06fcbefa32fba35
LuisaP28 <151669754+LuisaP28@users.noreply.github.com>
2024-09-08 21:08:54 -0500

    Creating the hexagonal structure

---
+++ src/main/java/com/riwi/complexus/ComplexusApplication.java
@@ -0,0 +1,13 @@
+package com.riwi.complexus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplexusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplexusApplication.class, args);
+	}
+
+}
