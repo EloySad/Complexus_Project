0562d002b8a7cb4268c8528e5b48a83f8d7ede31
LuisaP28 <151669754+LuisaP28@users.noreply.github.com>
2024-09-09 21:30:04 -0500

    DTos of the Notifications entity is created

---
+++ src/main/java/com/riwi/complexus/api/dto/request/NotificationRequest.java
@@ -0,0 +1,19 @@
+package com.riwi.complexus.api.dto.request;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class NotificationRequest {
+    private String message; 
+    private LocalDateTime date;
+}
