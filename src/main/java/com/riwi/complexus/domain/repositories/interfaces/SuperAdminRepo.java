b88fb8ba59c73845c62f90e38c962f3b71fe1879
Davi <daviocassio95@gmail.com>
2024-09-11 20:08:50 -0500

    Feat: superadmin files were deleted

--- src/main/java/com/riwi/complexus/domain/repositories/interfaces/SuperAdminRepo.java
+++
@@ -1,7 +0,0 @@
-package com.riwi.complexus.domain.repositories.interfaces;
-
-import com.riwi.complexus.domain.entities.SuperAdminEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SuperAdminRepo extends JpaRepository<SuperAdminEntity, Long> {
-}
