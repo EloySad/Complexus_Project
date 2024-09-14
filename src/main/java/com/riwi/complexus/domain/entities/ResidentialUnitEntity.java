c1adab69bb23b6314daab33648356400c436dee2
Davi <daviocassio95@gmail.com>
2024-09-14 14:59:09 -0500

    Create CRUD  of Residential unit

---
+++ src/main/java/com/riwi/complexus/domain/entities/ResidentialUnitEntity.java
@@ -0,0 +1,31 @@
+package com.riwi.complexus.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "residential_unit")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ResidentialUnitEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = false)
+    private String adress;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private UserEntity user;
+}
---
+++ src/main/java/com/riwi/complexus/domain/repositories/interfaces/ResidentialUnitRepo.java
@@ -0,0 +1,7 @@
+package com.riwi.complexus.domain.repositories.interfaces;
+
+import com.riwi.complexus.domain.entities.ResidentialUnitEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ResidentialUnitRepo extends JpaRepository<ResidentialUnitEntity, Long> {
+}
