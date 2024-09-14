a98f39f9077798d4708eff0a813fc4267f480568
EloySad <johaneloysanchezhernandez@gmail.com>
2024-09-14 16:20:08 -0500

    feat: config aplication.properties

---
+++ src/main/java/com/riwi/complexus/domain/entities/ResidentEntity.java
@@ -0,0 +1,32 @@
+package com.riwi.complexus.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "residents")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ResidentEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String tower;
+
+    @Column(nullable = false, length = 100)
+    private String residentialNumber;
+
+    @Column
+    private Boolean allowedNotification ;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private UserEntity userId;
+}
