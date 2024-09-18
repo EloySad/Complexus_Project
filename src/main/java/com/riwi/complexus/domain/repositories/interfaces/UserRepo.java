80bc13fc377b48deb5858e0af30e2b84cce1cadd
user <>
2024-09-17 21:13:22 -0500

    Fix bug

---
+++ src/main/java/com/riwi/complexus/domain/repositories/interfaces/UserRepo.java
@@ -0,0 +1,8 @@
+package com.riwi.complexus.domain.repositories.interfaces;
+
+import com.riwi.complexus.domain.entities.UserEntity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<UserEntity, Long> {
+}
