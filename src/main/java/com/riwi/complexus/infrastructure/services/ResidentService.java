0ed3865024ca05c6d1bd5231331472098b7d7866
EloySad <johaneloysanchezhernandez@gmail.com>
2024-09-17 19:27:44 -0500

    feat: saved changes

--- src/main/java/com/riwi/complexus/infrastructure/services/ResidentService.java
+++ src/main/java/com/riwi/complexus/infrastructure/services/ResidentService.java
@@ -3,11 +3,14 @@ package com.riwi.complexus.infrastructure.services;
 import com.riwi.complexus.api.dto.request.ResidentDto;
 import com.riwi.complexus.domain.entities.ResidentEntity;
 import com.riwi.complexus.domain.entities.ResidentialUnitEntity;
+import com.riwi.complexus.domain.entities.RolsEntity;
 import com.riwi.complexus.domain.entities.UserEntity;
 import com.riwi.complexus.domain.repositories.interfaces.ResidentRepo;
 import com.riwi.complexus.domain.repositories.interfaces.ResidentialUnitRepo;
 import com.riwi.complexus.domain.repositories.interfaces.UserRepo;
 import com.riwi.complexus.infrastructure.abstract_services.interfaces.IResidentService;
+import jakarta.validation.Valid;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -22,6 +25,9 @@ public class ResidentService implements IResidentService {
 
     @Autowired
     ResidentRepo residentRepo;
+
+    @Autowired
+    RolService rolService;
 
     @Autowired
     UserRepo userRepo;
@@ -46,27 +52,44 @@ public class ResidentService implements IResidentService {
     }
 
     @Override
-    public ResponseEntity<ResidentDto> create(ResidentDto entity) {
+    public ResponseEntity<ResidentDto> create(@Valid ResidentDto entity) {
         // 1. Check if residential unit exists
         ResidentialUnitEntity residentialUnit = residentialUnitRepo.findById(entity.getResidentialUnitId().getId())
                 .orElseThrow(() -> new RuntimeException("ResidentialUnitId Not Found"));
+
+        RolsEntity rol = rolService.readById(entity.getRoleId());
+        if (rol == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         // 2. Create user
 
-        // 3. Create resident with the created user
-        // createdResident.setUser(createdUser)
-
-
-
-        ResidentEntity resident = ResidentEntity.builder()
-                .tower(entity.getTower())
-                .residentialNumber(entity.getResidentialNumber())
-                .allowedNotification(entity.getAllowedNotification())
-                .userId(user)
-                .residentialUnitId(residentialUnit)
+        UserEntity newUser = UserEntity.builder()
+                .name(entity.getName())
+                .lastname(entity.getLastname)
+                .email(entity.getEmail())
+                .password(passwordEncoder.encode(entity.getPassword()))
+                .phone(entity.getPhone())
+                .role(rol)
                 .build();
 
-        ResidentEntity saveResident = residentRepo.save(resident);
-        return ResponseEntity.status(HttpStatus.OK).body(saveResident);
+        UserEntity createNewUser = userRepo.save(newUser);
+
+        // 3. Create resident with the created user
+        // clue:createdResident.setUser(createdUser)
+
+
+
+
+
+//        ResidentEntity resident = ResidentEntity.builder()
+//                .tower(entity.getTower())
+//                .residentialNumber(entity.getResidentialNumber())
+//                .allowedNotification(entity.getAllowedNotification())
+//                .userId(user)
+//                .residentialUnitId(residentialUnit)
+//                .build();
+//
+//        ResidentDto createdResident = residentRepo.save(resident);
+//        return ResponseEntity.status(HttpStatus.OK).body(saveResident);
     }
 
     @Override
