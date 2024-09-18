56615bea5b4d73382d3ac9890a9ca0894b011d17
Davi <daviocassio95@gmail.com>
2024-09-17 19:33:05 -0500

    Update files

--- src/main/java/com/riwi/complexus/domain/repositories/interfaces/UserRepo.java
+++ src/main/java/com/riwi/complexus/domain/repositories/interfaces/UserRepo.java
@@ -3,5 +3,5 @@ package com.riwi.complexus.domain.repositories.interfaces;
 import com.riwi.complexus.domain.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepo extends JpaRepository<UserEntity, Long> {
+public interface UserRepo extends JpaRepository <UserEntity, Long> {
 }
